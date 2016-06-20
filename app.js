90a6557e7f8d66a843b54b7f06a4c404fe7e25c6
alexlecco <alexlecco24@gmail.com>
2016-06-19 22:36:04 -0300

    [agregar tareas post method was added in index.js]

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-require('./models/Tareas');
-
 mongoose.connect('mongodb://localhost/tareas')
+
+require('.models/Tareas');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
--- routes/index.js
+++ routes/index.js
@@ -14,8 +14,16 @@ var Tareas = mongoose.model('Tareas'); // Tareas es el modelo de datos
 router.get('/tareas', function(req, res, next) {
   Tareas.find(function(err, tareas) {
     if(err) { return next(err) }
-
     res.json(tareas)
+  })
+})
+
+//POST - agregar tarea
+router.post('/tarea', function(req, res, next) {
+  var tarea = new Tareas(req.body);
+  tarea.save(function(err, tarea) {
+    if(err) { return next(err) }
+    res.json(tarea);
   })
 })
 
